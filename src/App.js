ad560f0a926d9439e23ea0b20c50ade555796906
Sara Elwakil <saraelwakil2021@icloud.com>
2022-02-17 12:20:41 +0200

    webpack

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
+import { json } from 'express';
+import React from 'react'
 import Confetti from "react-confetti"
 import Block from "./components/Block";
 
 
 
 function App() {
+ 
+  const randomNumber =(x)=> Math.floor(Math.random()*x)+1
+  const [tenzies, setTenzies] = React.useState(false)
+  const [values, setValues] = React.useState(Array.from({length: 10},(ele, i)=>({id:i,value:randomNumber(6),held:false})))
+  const BestRecord = localStorage.getItem("bestRecord")
+  const [rollRecord ,setRollRecord] = React.useState({rollCount:0,rollDuration:"ccc"})
+  const hold =(e ,value, key)=>{
+    setValues(prevValues=>{
+      return prevValues.map(p=>p.id===key? {...prevValues[key],held:true} : p)
+    })
+  }
+  const roll = ()=>{
+    if(!tenzies){
 
-  const dices= []
-  for(let i=0;i<10;i++){
-    dices.push(
-      <Block value={Math.floor(Math.random()*6)+1}/>
-    )
+      setValues(prevValues=> {
+        return prevValues.map(v=> v.held ? v : {...v,value : randomNumber(6)})
+      })
+      setRollRecord(prev=>({...prev,rollCount:prev.rollCount+1}))
+      console.log(rollRecord.rollCount)
+      //هنا كنت ممكن يكسب قبل ما يختار اخر مربع شبههم فعشان كدا عملت && عشان اتاكد ان الكل اختاار
+      if (values.every((v)=>v.value===values[0].value && v.held === values[0].held)){
+        setRollRecord(prev=>({...prev,rollCount:prev.rollCount}))
+        
+        console.log("wins")
+        setTenzies(true)
+      }
+    }else{
+      setRollRecord(prev=>({
+        ...prev,rollCount:0
+      }))
+      setTenzies(false)
+      setValues(Array.from({length: 10},(ele, i)=>({id:i,value:randomNumber(6),held:false})))
+    }
   }
   
 
+  const dices= values.map((v)=>(<Block key = {v.id} value={v.value} hold = {hold}  id = {v.id} held = {v.held}/>))
+
+
   return (
     <main>
-      <Confetti/>
+      {tenzies && <Confetti/>}
     <h1>Tenzies</h1>
     <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
     <div className="die-container">{dices}</div>
-    <button className="roll-dice" >
-        "Reset Game"
+    <button className="roll-dice" onClick={roll} >
+        {tenzies? "Reset Game" : "Roll" }
     </button>
+    <h3>your roll count : {rollRecord.rollCount}</h3>
 </main>
   );
 }
-
 export default App;
--- src/components/Block.js
+++ src/components/Block.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-const Block = ({held,hold, value}) => {
+const Block = ({hold, value, id, held}) => {
+
+    
     const styles = {
         backgroundColor: held ? "#59E391" : "white"
     }
-  return (
-    <div className="die-face" onClick={hold} style={styles}>
-        <h2 className="die-num">{value}</h2>
-    </div>
-  )
-}
+    return (
+        <div className="die-face" onClick={e=>hold(e,value, id )} style={styles}>
+            <h2 className="die-num">{value}</h2>
+        </div>
+    )
+    }
 
 export default Block
