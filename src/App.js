f56d0bceb2f84a21405b4baed85eb367decbd4ef
Sara Elwakil <saraelwakil2021@icloud.com>
2022-02-18 02:19:23 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { json } from 'express';
 import React from 'react'
 import Confetti from "react-confetti"
 import Block from "./components/Block";
@@ -10,29 +9,45 @@ function App() {
   const randomNumber =(x)=> Math.floor(Math.random()*x)+1
   const [tenzies, setTenzies] = React.useState(false)
   const [values, setValues] = React.useState(Array.from({length: 10},(ele, i)=>({id:i,value:randomNumber(6),held:false})))
-  const BestRecord = localStorage.getItem("bestRecord")
+  const [startTime, setStartTime] = React.useState(null)
+  const bestRecord = JSON.parse(localStorage.getItem("bestRecord")) || {}
+  let endTime
+  let diff
   const [rollRecord ,setRollRecord] = React.useState({rollCount:0,rollDuration:"ccc"})
+  
+console.log(values)
   const hold =(e ,value, key)=>{
     setValues(prevValues=>{
       return prevValues.map(p=>p.id===key? {...prevValues[key],held:true} : p)
     })
   }
   const roll = ()=>{
-    if(!tenzies){
+    
 
+    if (!startTime) {setStartTime(new Date().getTime()) }
+
+    if(!tenzies){
       setValues(prevValues=> {
         return prevValues.map(v=> v.held ? v : {...v,value : randomNumber(6)})
       })
       setRollRecord(prev=>({...prev,rollCount:prev.rollCount+1}))
-      console.log(rollRecord.rollCount)
       //هنا كنت ممكن يكسب قبل ما يختار اخر مربع شبههم فعشان كدا عملت && عشان اتاكد ان الكل اختاار
       if (values.every((v)=>v.value===values[0].value && v.held === values[0].held)){
-        setRollRecord(prev=>({...prev,rollCount:prev.rollCount}))
-        
-        console.log("wins")
+        endTime =new Date().getTime()
+        diff = endTime-startTime
+        setRollRecord(prev=>({...prev,rollCount:rollRecord.rollCount,rollDuration: diff}))
+
+        localStorage.setItem("bestRecord",
+                    JSON.stringify(
+                      {...bestRecord,
+                        rollCount:diff<bestRecord.rollDuration?rollRecord.rollCount : bestRecord.rollCount ,
+                        rollDuration: diff<bestRecord.rollDuration? diff : bestRecord.rollDuration
+                      }))
+
         setTenzies(true)
       }
     }else{
+      setStartTime(null)
       setRollRecord(prev=>({
         ...prev,rollCount:0
       }))
@@ -40,22 +55,24 @@ function App() {
       setValues(Array.from({length: 10},(ele, i)=>({id:i,value:randomNumber(6),held:false})))
     }
   }
-  
-
   const dices= values.map((v)=>(<Block key = {v.id} value={v.value} hold = {hold}  id = {v.id} held = {v.held}/>))
 
 
   return (
     <main>
       {tenzies && <Confetti/>}
-    <h1>Tenzies</h1>
-    <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-    <div className="die-container">{dices}</div>
-    <button className="roll-dice" onClick={roll} >
-        {tenzies? "Reset Game" : "Roll" }
-    </button>
-    <h3>your roll count : {rollRecord.rollCount}</h3>
-</main>
+      <h1>Tenzies</h1>
+      <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className="die-container">{dices}</div>
+      <button className="roll-dice" onClick={roll} >
+          {tenzies? "Reset Game" : "Roll" }
+      </button>
+      <div>
+        <h3>Your Roll count : {rollRecord.rollCount} Rolls</h3>
+        {tenzies && <h3>You spent: {` ${Math.floor(rollRecord.rollDuration %(1000*3600)/(1000*60))} mins ${Math.floor(rollRecord.rollDuration%(1000*60)/(1000)) } seconds`} </h3>}
+        <h3>Your best Records {`${bestRecord.rollCount} Rolls in  ${` ${Math.floor(bestRecord.rollDuration %(1000*3600)/(1000*60))} mins ${Math.floor(bestRecord.rollDuration%(1000*60)/(1000)) } seconds`}`}</h3>
+      </div>
+    </main>
   );
 }
 export default App;
--- src/components/Block.js
+++ src/components/Block.js
@@ -2,15 +2,83 @@ import React from 'react'
 
 const Block = ({hold, value, id, held}) => {
 
-    
+    console.log(value)
+
     const styles = {
         backgroundColor: held ? "#59E391" : "white"
     }
+    let dice 
+        switch (value){
+
+            case 1:
+            dice= (<div className="first-face">
+                    <span className="pip"></span>
+            </div>)
+            break;
+            case 2:
+                dice= (<div className="second-face">
+                    <span className="pip"></span>
+                    <span className="pip"></span>
+                </div>)
+                break;
+            case 3:
+                dice=  (<div className="third-face">
+                        <span className="pip"></span>
+                        <span className="pip"></span>
+                        <span className="pip"></span>
+                </div>)
+                break;
+            case 4:
+                dice= (<div className="fourth-face">
+                        <div className="column">
+                        <span className="pip"></span>
+                        <span className="pip"></span>
+                        </div>
+                        <div className="column">
+                        <span className="pip"></span>
+                        <span className="pip"></span>
+                        </div>
+                    </div>)
+                break;
+            case 5:
+                dice=   (<div className="fifth-face">
+                        <div className="column">
+                        <span className="pip"></span>
+                        <span className="pip"></span>
+                        </div>
+                        <div className="column">
+                        <span className="pip"></span>
+                        </div>
+                        <div className="column">
+                        <span className="pip"></span>
+                        <span className="pip"></span>
+                        </div>
+                    </div>)
+                break;
+            case 6:
+                dice=  (<div className="sixth-face">
+                        <div className="column">
+                        <span className="pip"></span>
+                        <span className="pip"></span>
+                        <span className="pip"></span>
+                        </div>
+                        <div className="column">
+                        <span className="pip"></span>
+                        <span className="pip"></span>
+                        <span className="pip"></span>
+                        </div>
+                    </div>)
+                break;
+            default:
+                return
+    }
+
     return (
-        <div className="die-face" onClick={e=>hold(e,value, id )} style={styles}>
-            <h2 className="die-num">{value}</h2>
+        <div  onClick={e=>hold(e,value, id )} style={styles}>
+            {dice}
         </div>
     )
-    }
+    
+}
 
 export default Block
