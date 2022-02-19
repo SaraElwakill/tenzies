f10daadda41433708b9a90bfefaf76dbea9cd178
Sara Elwakil <saraelwakil2021@icloud.com>
2022-02-19 16:27:38 +0200

    update play

--- src/App.js
+++ src/App.js
@@ -10,55 +10,61 @@ function App() {
   const [tenzies, setTenzies] = React.useState(false)
   const [values, setValues] = React.useState(Array.from({length: 10},(ele, i)=>({id:i,value:randomNumber(6),held:false})))
   const [startTime, setStartTime] = React.useState(null)
-  const bestRecord = JSON.parse(localStorage.getItem("bestRecord")) || {rollCount:0,rollDuration:0}
-  let endTime
+  let bestRecord= JSON.parse(localStorage.getItem("bestRecord")) || rollRecord
+  const [rollRecord ,setRollRecord] = React.useState({rollCount:0,rollDuration:0})
   let diff
-  const [rollRecord ,setRollRecord] = React.useState({rollCount:0,rollDuration:"ccc"})
+  let endTime
   
-console.log(values)
   const hold =(e ,value, key)=>{
-    setValues(prevValues=>{
-      return prevValues.map(p=>p.id===key? {...p,held:!p.held} : p)
-    })
+
+    
+    if (values.every((v)=>v.value===values[0].value)){
+      setValues(prevValues=>{
+        return prevValues.map(p=>p.id===key? {...p,held:!p.held} : p)
+      })
+      setTenzies(true)
+      endTime =new Date().getTime()
+      diff = endTime-startTime
+      setRollRecord(prev=>({...prev,rollCount:rollRecord.rollCount,rollDuration: diff}))
+      console.log(bestRecord)
+      console.log(diff)
+      localStorage.setItem("bestRecord",
+      JSON.stringify(
+        {...bestRecord,
+          rollCount:diff<bestRecord.rollDuration?rollRecord.rollCount : bestRecord.rollCount ,
+          rollDuration: diff<bestRecord.rollDuration? diff : bestRecord.rollDuration
+        }))
+      }else{
+        setValues(prevValues=>{
+          return prevValues.map(p=>p.id===key? {...p,held:!p.held} : p)
+        })
+      }
+    
+    
   }
   const roll = ()=>{
     
-
+    
     if (!startTime) {setStartTime(new Date().getTime()) }
-
+    
     if(!tenzies){
       setValues(prevValues=> {
         return prevValues.map(v=> v.held ? v : {...v,value : randomNumber(6)})
       })
       setRollRecord(prev=>({...prev,rollCount:prev.rollCount+1}))
-      //هنا كنت ممكن يكسب قبل ما يختار اخر مربع شبههم فعشان كدا عملت && عشان اتاكد ان الكل اختاار
-      if (values.every((v)=>v.value===values[0].value && v.held === values[0].held)){
-        endTime =new Date().getTime()
-        diff = endTime-startTime
-        setRollRecord(prev=>({...prev,rollCount:rollRecord.rollCount,rollDuration: diff}))
-
-        localStorage.setItem("bestRecord",
-                    JSON.stringify(
-                      {...bestRecord,
-                        rollCount:diff<bestRecord.rollDuration?rollRecord.rollCount : bestRecord.rollCount ,
-                        rollDuration: diff<bestRecord.rollDuration? diff : bestRecord.rollDuration
-                      }))
-
-        setTenzies(true)
-      }
     }else{
-      setStartTime(null)
-      setRollRecord(prev=>({
-        ...prev,rollCount:0
-      }))
-      setTenzies(false)
-      setValues(Array.from({length: 10},(ele, i)=>({id:i,value:randomNumber(6),held:false})))
-    }
+        setStartTime(null)
+        setRollRecord(prev=>({
+          ...prev,rollCount:0
+        }))
+        setTenzies(false)
+        setValues(Array.from({length: 10},(ele, i)=>({id:i,value:randomNumber(6),held:false})))
+      }
+    
   }
-  const dices= values.map((v)=>(<Block key = {v.id} value={v.value} hold = {hold}  id = {v.id} held = {v.held}/>))
+    const dices= values.map((v)=>(<Block key = {v.id} value={v.value} hold = {hold}  id = {v.id} held = {v.held}/>))
 
-
-  return (
+    return (
     <main>
       {tenzies && <Confetti/>}
       <h1>Tenzies</h1>
